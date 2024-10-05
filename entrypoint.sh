434962339fcb19dcabf7ef9d96ff6b8d8c9ad23c
tanlda <ledinhanhtan.stack@gmail.com>
2024-10-05 13:46:33 +0700

    feat: fork

--- entrypoint.sh
+++ entrypoint.sh
@@ -70,37 +70,13 @@ echo "[+] Cloning destination git repository $DESTINATION_REPOSITORY_NAME"
 git config --global user.email "$USER_EMAIL"
 git config --global user.name "$USER_NAME"
 
-# workaround for https://github.com/cpina/github-action-push-to-another-repository/issues/103
 git config --global http.version HTTP/1.1
 
-{
-	git clone --single-branch --depth 1 --branch "$TARGET_BRANCH" "$GIT_CMD_REPOSITORY" "$CLONE_DIR"
-} || {
-    if [ "$CREATE_TARGET_BRANCH_IF_NEEDED" = "true" ]
-    then
-        # Default branch of the repository is cloned. Later on the required branch
-	# will be created
-        git clone --single-branch --depth 1 "$GIT_CMD_REPOSITORY" "$CLONE_DIR"
-    else
-        false
-    fi
-} || {
-	echo "::error::Could not clone the destination repository. Command:"
-	echo "::error::git clone --single-branch --branch $TARGET_BRANCH $GIT_CMD_REPOSITORY $CLONE_DIR"
-	echo "::error::(Note that if they exist USER_NAME and API_TOKEN is redacted by GitHub)"
-	echo "::error::Please verify that the target repository exist AND that it contains the destination branch name, and is accesible by the API_TOKEN_GITHUB OR SSH_DEPLOY_KEY"
-	exit 1
-
-}
 ls -la "$CLONE_DIR"
 
 TEMP_DIR=$(mktemp -d)
-# This mv has been the easier way to be able to remove files that were there
-# but not anymore. Otherwise we had to remove the files from "$CLONE_DIR",
-# including "." and with the exception of ".git/"
 mv "$CLONE_DIR/.git" "$TEMP_DIR/.git"
 
-# $TARGET_DIRECTORY is '' by default
 ABSOLUTE_TARGET_DIRECTORY="$CLONE_DIR/$TARGET_DIRECTORY/"
 
 echo "[+] Deleting $ABSOLUTE_TARGET_DIRECTORY"
@@ -147,18 +123,7 @@ COMMIT_MESSAGE="${COMMIT_MESSAGE/ORIGIN_COMMIT/$ORIGIN_COMMIT}"
 COMMIT_MESSAGE="${COMMIT_MESSAGE/\$GITHUB_REF/$GITHUB_REF}"
 
 echo "[+] Set directory is safe ($CLONE_DIR)"
-# Related to https://github.com/cpina/github-action-push-to-another-repository/issues/64
 git config --global --add safe.directory "$CLONE_DIR"
-
-if [ "$CREATE_TARGET_BRANCH_IF_NEEDED" = "true" ]
-then
-    echo "[+] Switch to the TARGET_BRANCH"
-    # || true: if the $TARGET_BRANCH already existed in the destination repo:
-    # it is already the current branch and it cannot be switched to
-    # (it's not needed)
-    # If the branch did not exist: it switches (creating) the branch
-    git switch -c "$TARGET_BRANCH" || true
-fi
 
 echo "[+] Adding git commit"
 git add .
@@ -167,9 +132,10 @@ echo "[+] git status:"
 git status
 
 echo "[+] git diff-index:"
-# git diff-index : to avoid doing the git commit failing if there are no changes to be commit
 git diff-index --quiet HEAD || git commit --message "$COMMIT_MESSAGE"
 
-echo "[+] Pushing git commit"
-# --set-upstream: sets de branch when pushing to a branch that does not exist
-git push "$GIT_CMD_REPOSITORY" --set-upstream "$TARGET_BRANCH"
+#echo "[+] Pushing git commit"
+#git push "$GIT_CMD_REPOSITORY" --set-upstream "$TARGET_BRANCH"
+
+echo "[+] Pushing git commit (Force)"
+git push $GIT_CMD_REPOSITORY" --set-upstream "$TARGET_BRANCH" --force
